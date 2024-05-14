29fcf47d62200ed1569a98f9b902a13b109e1ce1
luka <102388689+F1R0o@users.noreply.github.com>
2024-05-14 21:21:11 +0400

    davaleba1

---
+++ davaleba1/main.js
@@ -0,0 +1,53 @@
+
+
+function func1(a,b) {
+    if (a === b) {
+        return "Tolia"
+    }
+    else{
+        return "ar aris tolia"
+    }
+
+}
+
+
+
+
+function func2(fahrenheit) {
+    if (typeof fahrenheit === 'number') {
+        let celsius = ((fahrenheit - 32) / 1.8).toFixed(2);
+        return celsius;
+    } else {
+        return "false";
+    }
+}
+
+
+
+function calculator(a, b, operation) {
+    if (typeof a !== "number" || typeof b !== 'number') {
+        return false;
+    }
+    
+    if (operation === "+") {
+        return a + b;
+    } else if (operation === "-") {
+        return a - b;
+    } else if (operation === "*") {
+        return a * b;
+    } else if (operation === "/") {
+       
+            return a / b ;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+let d = calculator(1,2,"/")
+console.log(d)
