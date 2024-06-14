c4432dc251482c3daa879830c342855c01225b62
luka <102388689+F1R0o@users.noreply.github.com>
2024-06-14 22:29:51 +0400

    dd

---
+++ davaleba11/firstreact/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>luka firuasvhili</code> pirveli react.
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          google.com
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
